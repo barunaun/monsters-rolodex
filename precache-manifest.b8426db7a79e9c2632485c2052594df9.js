1d08b4395e9be6a0ca8c9cae22a26ea476f5eb01
Baruna <Barunaadiharto@gmail.com>
2019-12-15 07:24:14 +0700

    Updates

---
+++ precache-manifest.b8426db7a79e9c2632485c2052594df9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7a95741c86f05d573141510f138ed6c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "963b686de45946c409e1",
+    "url": "/monsters-rolodex/static/css/main.17c696be.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "963b686de45946c409e1",
+    "url": "/monsters-rolodex/static/js/main.fcbc69aa.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
